fad0fe6feb5547078d7f74a9e0ea6354526e9320
SHANIMCD <shanilmcdonald@gmail.com>
2020-07-27 16:43:17 +0100

    test 'currently showing' list complete

---
+++ secretcinema/src/test/testCurrentlyShowIndex.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+// const app = require('./app');
+const apiKey = '&apikey=f1887e96';
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Showing', () => {
+    describe('GET /showing', function() {
+        it('should return an array of currently showing movies', (done) => {
+            chai.request('http://www.omdbapi.com')
+            .get('/?s=harry+potter' + apiKey)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res).to.be.an('object');
+                done();
+            })
+        })
+    })
+});
