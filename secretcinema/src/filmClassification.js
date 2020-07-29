4dd527430e52f783ae450723ee6d4bc4717f987e
Naomi Gabriel <naomi_gab@hotmail.com>
2020-07-29 15:15:46 +0100

    meh

--- secretcinema/src/filmClassification.js
+++ secretcinema/src/filmClassification.js
@@ -5,63 +5,73 @@ class filmClassification extends React.Component {
         return (
 
             <div class="container" class="Lphoto">
-                <h1 >Standard Film Classifications</h1>
-                <div class="row">
-
+                <h1> Film Classifications</h1>
+                <div class="row pad-top-bot-20">
+                    
 
                     <div class="col-md-6">
 
                         <div class="row">
-                            <div class="col-md-4">
-                                <img class="classifications" alt='Universal' width="100" height="100" src="https://vignette.wikia.nocookie.net/rating-system/images/8/88/BBFC_U.svg/revision/latest/scale-to-width-down/340?cb=20191126211352" />
-                                <h4>Universal</h4>
-                            </div>
-                            <div class="col-md-8">
-                                Suitable for all.
-            <br /> (The board states that while they cannot predict what might upset a particular child;
-            <br /> a 'U' film should be suitable for audiences aged 4 and older).
-            <br /> <a href="https://www.bbfc.co.uk/about-classification/u" target="_blank">More Information</a>
-                            </div>
 
-                            <div class="col-md-4">
-                                <img class="classifications" alt='Universal' width="100" height="100" src="https://vignette.wikia.nocookie.net/rating-system/images/8/88/BBFC_U.svg/revision/latest/scale-to-width-down/340?cb=20191126211352" />
-                                <h4>Universal</h4>
-                            </div>
-                            <div class="col-md-8">
-                                Suitable for all.
-            <br /> (The board states that while they cannot predict what might upset a particular child;
-            <br /> a 'U' film should be suitable for audiences aged 4 and older).
-            <br /> <a href="https://www.bbfc.co.uk/about-classification/u" target="_blank">More Information</a>
-                            </div>
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='12A' width="100" height="100" src="https://img4.wikia.nocookie.net/__cb20100225224531/gtawiki/images/7/7a/BBFC_12A_Rating.png" />
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>12 Advisory</h4>
+                                    Recommended for 12 years and older.
+                                    </div>
+                                </div>
 
-
+                            </div>
+                            <div class="col-md-6">
+                            <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='Parental Guidance' width="100" height="100" src="https://images2.wikia.nocookie.net/__cb20100225224403/gtawiki/images/2/21/BBFC_PG.png" />
+                     
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>Parental Guidance
+                                    </h4>
+                                    General viewing, but some scenes may be unsuitable for young children.
+          
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-
 
 
                     </div>
                     <div class="col-md-6">
-                       < div class="row">
-                            <div class="col-md-4">
-                            <img class="classifications" alt='Universal' width="100" height="100" src="https://vignette.wikia.nocookie.net/rating-system/images/8/88/BBFC_U.svg/revision/latest/scale-to-width-down/340?cb=20191126211352" />
-                            <h4>Universal</h4>
-                        </div>
-                        <div class="col-md-8">
-                            Suitable for all.
-            <br /> (The board states that while they cannot predict what might upset a particular child;
-            <br /> a 'U' film should be suitable for audiences aged 4 and older).
-            <br /> <a href="https://www.bbfc.co.uk/about-classification/u" target="_blank">More Information</a>
-                        </div>
 
-                        <div class="col-md-4">
-                            <img class="classifications" alt='Universal' width="100" height="100" src="https://vignette.wikia.nocookie.net/rating-system/images/8/88/BBFC_U.svg/revision/latest/scale-to-width-down/340?cb=20191126211352" />
-                            <h4>Universal</h4>
-                        </div>
-                        <div class="col-md-8">
-                            Suitable for all.
-            <br /> (The board states that while they cannot predict what might upset a particular child;
-            <br /> a 'U' film should be suitable for audiences aged 4 and older).
-            <br /> <a href="https://www.bbfc.co.uk/about-classification/u" target="_blank">More Information</a>
+                        <div class="row">
+
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='15' width="100" height="100" src="https://cdn.thinglink.me/api/image/512616823280107522/1240/10/scaletowidth" />
+                       
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>15</h4>
+                                    Suitable only for 15 years and older. Nobody younger than 15 may see a '15' film in a cinema. Nobody
+                                               </div>
+                                </div>
+
+                            </div>
+                            <div class="col-md-6">
+                            <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='18' width="100" height="100" src="https://cdn.thinglink.me/api/image/512616823280107522/1240/10/scaletowidth" />
+                        
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>18</h4>
+                                    Suitable only for adults. Nobody younger than 18 may see an '18' film in a cinema. Nobody younger than
+                                                </div>
+                                </div>
+                            </div>
                         </div>
 
 
@@ -70,13 +80,85 @@ class filmClassification extends React.Component {
 
 
 
-
-
-
-                    <br />
                 </div>
+                 
+                <div class="row pad-top-bot-20">
+                
+
+                    <div class="col-md-6">
+
+                        <div class="row">
+
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='R18' width="100" height="100" src="https://aphweblog.files.wordpress.com/2011/06/ca154-200px-oflc_small_r18252b_svg.png" />
+                                                           </div>
+                                    <div class="col-md-8">
+                                    <h4>R18</h4>
+                                    To be shown only in specially licensed sex cinemas, or supplied only in licensed sex
+                                               </div>
+                                </div>
+
+                            </div>
+                            <div class="col-md-6">
+                            <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='12A' width="100" height="100" src="https://img4.wikia.nocookie.net/__cb20100225224531/gtawiki/images/7/7a/BBFC_12A_Rating.png" />
+
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>12 Advisory</h4>
+                                    Recommended for 12 years and older.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="col-md-6">
+
+                        <div class="row">
+
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='12A' width="100" height="100" src="https://img4.wikia.nocookie.net/__cb20100225224531/gtawiki/images/7/7a/BBFC_12A_Rating.png" />
+
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>12 Advisory</h4>
+                                    Recommended for 12 years and older.
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col-md-6">
+                            <div class="row">
+                                    <div class="col-md-4 no-padding">
+                                    <img class="classifications" alt='12A' width="100" height="100" src="https://img4.wikia.nocookie.net/__cb20100225224531/gtawiki/images/7/7a/BBFC_12A_Rating.png" />
+
+                                    </div>
+                                    <div class="col-md-8">
+                                    <h4>12 Advisory</h4>
+                                    Recommended for 12 years and older.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+
+
+                </div>
+
+
             </div>
-            </div>
+
 
 
 
